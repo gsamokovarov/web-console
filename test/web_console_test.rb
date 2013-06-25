a59ea26bfe238e1402c7b50c2881c68909e5299b
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-06-25 11:24:39 +0300

    Refactor the engine tests

--- test/web_console_test.rb
+++ test/web_console_test.rb
@@ -1,48 +1,34 @@
-require 'rails'
-require 'action_controller/railtie'
-require 'active_support/dependencies'
-require 'tzinfo'
 require 'test_helper'
 
 class WebConsoleTest < ActiveSupport::TestCase
-  @@default_mount_path = nil
-  class << self
-    private
-
-      def default_mount_path(path)
-        @@default_mount_path = path
-      end
-  end
-
-  ROOT = File.expand_path('../dummy', __FILE__)
-  FIXTURES_PATH = File.expand_path('../fixtures', __FILE__)
-
-  def setup
-    FileUtils.mkdir_p ROOT
-    Dir.chdir ROOT
-
-    @old_app = Rails.application
-    Rails.application = nil
-
-    @app = Class.new(Rails::Application)
-    @app.config.eager_load = false
-    @app.config.time_zone = 'UTC'
-    @app.config.middleware ||= Rails::Configuration::MiddlewareStackProxy.new
-    @app.config.active_support.deprecation = :notify
-
-    if @@default_mount_path
-      @app.config.web_console.default_mount_path = @@default_mount_path
-    end
-
-    @app.initialize!
-  end
-
-  def teardown
-    Rails.application = @old_app
-  end
-
-  default_mount_path '/shell'
   test 'different default_mount_path' do
-    @app.config.web_console.default_mount_path
+    new_uninitialized_app do |app|
+      app.config.web_console.default_mount_path = '/shell'
+      app.initialize!
+
+      assert app.routes.named_routes['web_console'].path.match('/shell')
+    end
   end
+
+  private
+
+    def new_uninitialized_app(root = File.expand_path('../dummy', __FILE__))
+      FileUtils.mkdir_p root
+      Dir.chdir root
+
+      old_app = Rails.application
+      Rails.application = nil
+
+      app = Class.new(Rails::Application)
+      app.config.eager_load = false
+      app.config.time_zone = 'UTC'
+      app.config.middleware ||= Rails::Configuration::MiddlewareStackProxy.new
+      app.config.active_support.deprecation = :notify
+
+      begin
+        yield app
+      ensure
+        Rails.application = old_app
+      end
+    end
 end
