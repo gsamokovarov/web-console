d87f922aa1ab67d65e62f8bf80a823fbe676fe94
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-07-01 15:27:49 +0300

    Initial outline of the REPL module

---
+++ lib/web_console/repl.rb
@@ -0,0 +1,35 @@
+require 'active_support/core_ext/module/attriute_accessors'
+
+module WebConsole
+  module REPL
+    mattr_accessor(:adapters) { Hash.new }
+
+    class << self
+      # Register an adapter into the adapters registry.
+      #
+      # Registration maps and adapter class to an existing REPL implementation,
+      # that we call an adaptee constant. If the adaptee constant is not given,
+      # it is automatically derived from the adapter class name.
+      #
+      # For example, adapter named `WebConsole::REPL::IRB` will derive the
+      # adaptee constant to `::IRB`.
+      def register_adapter(adapter_class, adaptee_constant = nil)
+        adaptee_constant ||= derive_adaptee_constant_from(adapter_class)
+        adapters[adaptee_constant] = adapter_class
+      end
+
+      # Get the default adapter for the given application.
+      #
+      # By default the application will be Rails.application and the adapter
+      # will be chosen from config.console.
+      def default(app = Rails.application)
+        adapters[app.config.console]
+      end
+
+      private
+        def derive_adaptee_constant_from(adapter_class)
+          "::#{adapter_class.split('::').last}".constantize
+        end
+    end
+  end
+end
