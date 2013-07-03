64e1f56aa732ed5e674da3398e5adb20025af11f
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-07-03 19:05:53 +0300

    Introduce session preservation test

--- test/web_console/repl/irb_test.rb
+++ test/web_console/repl/irb_test.rb
@@ -10,6 +10,12 @@ class IRBTest < ActiveSupport::TestCase
     assert_equal sprintf(return_prompt, "42\n"), irb.send_input('foo = 42')
   end
 
+  test 'preserves the session in the binding' do
+    irb = WebConsole::REPL::IRB.new
+    assert_equal sprintf(return_prompt, "42\n"), irb.send_input('foo = 42')
+    assert_equal sprintf(return_prompt, "50\n"), irb.send_input('foo + 8')
+  end
+
   private
     def return_prompt
       ::IRB.conf[:PROMPT][::IRB.conf[:PROMPT_MODE]][:RETURN]
