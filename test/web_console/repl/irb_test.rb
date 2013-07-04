ca8a22c1f2a714bec93732a42a0b6cd187c6b66b
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-07-04 12:51:25 +0300

    Add test for IRB session preservation

--- test/web_console/repl/irb_test.rb
+++ test/web_console/repl/irb_test.rb
@@ -16,11 +16,18 @@ class IRBTest < ActiveSupport::TestCase
     assert_equal sprintf(return_prompt, "50\n"), irb.send_input('foo + 8')
   end
 
-  test 'session isolation' do
+  test 'session isolation requires own bindings' do
     irb1 = WebConsole::REPL::IRB.new(Object.new.instance_eval { binding })
     irb2 = WebConsole::REPL::IRB.new(Object.new.instance_eval { binding })
     assert_equal sprintf(return_prompt, "42\n"), irb1.send_input('foo = 42')
-    assert_match /undefined local variable or method `foo'/, irb2.send_input('foo')
+    assert_match %r{undefined local variable or method `foo'}, irb2.send_input('foo')
+  end
+
+  test 'session preservation requires same bindings' do
+    irb1 = WebConsole::REPL::IRB.new
+    irb2 = WebConsole::REPL::IRB.new
+    assert_equal sprintf(return_prompt, "42\n"), irb1.send_input('foo = 42')
+    assert_equal sprintf(return_prompt, "42\n"), irb2.send_input('foo')
   end
 
   private
