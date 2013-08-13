4fdb446869b84c2efa941815458fb6c40477fed0
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-08-13 14:22:07 +0300

    Actualize the controller tests

--- test/controllers/web_console/console_sessions_controller_test.rb
+++ test/controllers/web_console/console_sessions_controller_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 module WebConsole
   class ConsoleSessionsControllerTest < ActionController::TestCase
     setup do
-      # Where does .stubs lives?
-      def @request.remote_ip; '127.0.0.1' end
+      @request.stubs(:remote_ip).returns('127.0.0.1')
     end
 
     test 'index is successful' do
@@ -12,14 +11,41 @@ module WebConsole
       assert_response :success
     end
 
-    test 'index creates new console session' do
+    test 'GET index creates new console session' do
       assert_difference 'ConsoleSession::INMEMORY_STORAGE.size' do
         get :index, use_route: 'web_console'
       end
     end
 
+    test 'PUT interrupt sends interrupts to the REPL' do
+      get :index, use_route: 'web_console'
+
+      assert_not_nil console_session = assigns(:console_session)
+      console_session.expects(:send_interrupt)
+
+      put :interrupt, id: console_session.pid, use_route: 'web_console'
+    end
+
+    test 'PUT inputs sends input to the REPL' do
+      get :index, use_route: 'web_console'
+
+      assert_not_nil console_session = assigns(:console_session)
+      console_session.expects(:send_input)
+
+      put :input, id: console_session.pid, use_route: 'web_console'
+    end
+
+    test 'GET pending_output gives the REPL pending output' do
+      get :index, use_route: 'web_console'
+
+      assert_not_nil console_session = assigns(:console_session)
+      console_session.expects(:pending_output)
+
+      get :pending_output, id: console_session.pid, use_route: 'web_console'
+    end
+
     test 'blocks requests from non-whitelisted ips' do
-      def @request.remote_ip; '128.0.0.1' end
+      @request.stubs(:remote_ip).returns('128.0.0.1')
       get :index, use_route: 'web_console'
       assert_response :unauthorized
     end
