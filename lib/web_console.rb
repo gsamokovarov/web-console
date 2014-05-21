fa2a5f4a57ea39c4446ca1b09aaaec43732bc73a
Ryan Dao <daoduyducduong@gmail.com>
2014-05-21 21:15:04 +0800

    Load the middleware

--- lib/web_console.rb
+++ lib/web_console.rb
@@ -1,3 +1,5 @@
+require 'web_console/error_page'
+require 'web_console/middleware'
 require 'active_support/lazy_load_hooks'
 require 'web_console/repl'
 require 'web_console/engine'
@@ -5,6 +7,11 @@ require 'web_console/colors'
 require 'web_console/slave'
 
 module WebConsole
+  class << self
+    attr_accessor :application_root
+    attr_accessor :logger
+  end
+
   # Shortcut the +WebConsole::Engine.config.web_console+.
   def self.config
     Engine.config.web_console
@@ -12,3 +19,7 @@ module WebConsole
 
   ActiveSupport.run_load_hooks(:web_console, self)
 end
+
+require "binding_of_caller"
+require "web_console/exception_extension"
+require 'web_console/rails' if defined? Rails::Railtie
