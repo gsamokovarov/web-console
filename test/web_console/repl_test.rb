45cea981f13042b2648d7dc0f09e644b49f68c9c
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-06-09 19:37:04 +0300

    Cleanup WebConsole::REPL tests
    They threw junk to the STD{OUT,ERR} and we no longer capture those.

--- test/web_console/repl_test.rb
+++ test/web_console/repl_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class REPLTest < ActiveSupport::TestCase
   setup do
     @repl1 = @repl = WebConsole::REPL.new
-    @repl2 = WebConsole::REPL.new
+    @repl2         = WebConsole::REPL.new
   end
 
   test 'sending input returns the result as output' do
@@ -15,24 +15,9 @@ class REPLTest < ActiveSupport::TestCase
     assert_equal "=> 50\n", @repl.send_input('foo + 8')
   end
 
-  # test 'session isolation requires own bindings' do
-  #   dummy1 = WebConsole::REPL::IRB.new(Object.new.instance_eval('binding'))
-  #   dummy2 = WebConsole::REPL::IRB.new(Object.new.instance_eval('binding'))
-  #   assert_equal "=> 42\n", dummy1.send_input('foo = 42')
-  #   assert_match %r{NameError}, dummy2.send_input('foo')
-  # end
-
   test 'session preservation requires same bindings' do
     assert_equal "=> 42\n", @repl1.send_input('foo = 42')
     assert_equal "=> 42\n", @repl2.send_input('foo')
-  end
-
-  test 'captures stdout output' do
-    assert_equal "=> nil\n", @repl.send_input('puts 42')
-  end
-
-  test 'captures stderr output' do
-    assert_equal "=> 3\n", @repl.send_input('$stderr.write("42\n")')
   end
 
   test 'prompt is present' do
