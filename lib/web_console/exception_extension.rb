85d8aaa064a22dd366dcd1754a7311ec5c708348
Ryan Dao <daoduyducduong@gmail.com>
2014-06-22 06:40:36 +0800

    Remove prepend_feature and monkey patch Exception class

--- lib/web_console/exception_extension.rb
+++ lib/web_console/exception_extension.rb
@@ -1,18 +1,21 @@
-module WebConsole
-  module ExceptionExtension
-    # TODO: remove prepend_features
-    prepend_features Exception
+class Exception
+  original_set_backtrace = instance_method(:set_backtrace)
 
-    def set_backtrace(*)
-      if caller_locations.none? { |loc| loc.path == __FILE__ }
+  # TODO: Check if binding_of_caller is available
+  define_method :set_backtrace do |*args|
+    unless Thread.current[:__web_console_exception_lock]
+      Thread.current[:__web_console_exception_lock] = true
+      begin
         @__web_console_bindings_stack = binding.callers.drop(1)
+      ensure
+        Thread.current[:__web_console_exception_lock] = false
       end
-
-      super
     end
 
-    def __web_console_bindings_stack
-      @__web_console_bindings_stack || []
-    end
+    original_set_backtrace.bind(self).call(*args)
+  end
+
+  def __web_console_bindings_stack
+    @__web_console_bindings_stack || []
   end
 end
