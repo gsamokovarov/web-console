3b9f9fe2986901156b49b6c0e4486a2c60d0abf8
Ryan Dao <daoduyducduong@gmail.com>
2014-07-17 22:13:20 +0700

    Test if the exception binding is correct

--- test/web_console/exception_extention_test.rb
+++ test/web_console/exception_extention_test.rb
@@ -6,9 +6,11 @@ class ExceptionExtensionTest < ActiveSupport::TestCase
 
   test 'should store binding trace if binding_of_caller is available' do
     begin
+      test = "Test"
       raise TestError
     rescue TestError => e
       assert e.__web_console_bindings_stack.length > 0
+      assert e.__web_console_bindings_stack[0].eval("test") == "Test"
     end
   end
 end
