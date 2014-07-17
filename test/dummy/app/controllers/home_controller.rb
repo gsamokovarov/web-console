ab18afea3625664d0f96522a2c1169df0ede54ee
Ryan Dao <daoduyducduong@gmail.com>
2014-07-17 22:44:57 +0700

    Demo the helper in Dummy app

--- test/dummy/app/controllers/home_controller.rb
+++ test/dummy/app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
   def index
     test = "Test"
+    @view_test = "View test"
     test_method
   end
 
   def test_method
     test2 = "Test2"
-    raise
+    # raise
   end
 end
