1852752e1e1fe2196454a32292a2f80e0119fc1a
Ryan Dao <daoduyducduong@gmail.com>
2014-07-19 15:11:51 +0700

    Use lazy load hooks in Railtie

--- lib/web_console/railtie.rb
+++ lib/web_console/railtie.rb
@@ -3,8 +3,13 @@ require "web_console/view_helpers"
 module WebConsole
   class Railtie < Rails::Railtie
     initializer "web_console.view_helpers" do
-      ActionView::Base.send :include, ViewHelpers
-      ActionController::Base.prepend_view_path File.dirname(__FILE__) + '/../action_dispatch/templates'
+      ActiveSupport.on_load :action_view do
+        include WebConsole::ViewHelpers
+      end
+
+      ActiveSupport.on_load :action_controller do
+        prepend_view_path File.dirname(__FILE__) + '/../action_dispatch/templates'
+      end
     end
   end
 end
