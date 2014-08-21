e9ddbcdc033c5aa5d64cbb76a0574c92b6453f84
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-08-21 16:03:39 +0300

    We depend on binding_of_caller, so it's always gonna be available

--- lib/web_console.rb
+++ lib/web_console.rb
@@ -1,7 +1,10 @@
+require 'binding_of_caller'
+
 require 'active_support/lazy_load_hooks'
 require 'action_dispatch/exception_wrapper'
 require 'action_dispatch/debug_exceptions'
 
+require 'web_console/exception_extension'
 require "web_console/view_helpers"
 require 'web_console/colors'
 require 'web_console/engine'
@@ -10,25 +13,10 @@ require 'web_console/repl_session'
 require 'web_console/slave'
 
 module WebConsole
-  class << self
-    attr_accessor :binding_of_caller_available
-
-    alias_method :binding_of_caller_available?, :binding_of_caller_available
-
-    # Shortcut the +WebConsole::Engine.config.web_console+.
-    def config
-      Engine.config.web_console
-    end
+  # Shortcut the +WebConsole::Engine.config.web_console+.
+  def self.config
+    Engine.config.web_console
   end
 
   ActiveSupport.run_load_hooks(:web_console, self)
 end
-
-begin
-  require 'binding_of_caller'
-  WebConsole.binding_of_caller_available = true
-rescue LoadError
-  WebConsole.binding_of_caller_available = false
-end
-
-require 'web_console/exception_extension'
--- lib/web_console/exception_extension.rb
+++ lib/web_console/exception_extension.rb
@@ -1,19 +1,17 @@
 class Exception
   original_set_backtrace = instance_method(:set_backtrace)
 
-  if WebConsole.binding_of_caller_available?
-    define_method :set_backtrace do |*args|
-      unless Thread.current[:__web_console_exception_lock]
-        Thread.current[:__web_console_exception_lock] = true
-        begin
-          @__web_console_bindings_stack = binding.callers.drop(1)
-        ensure
-          Thread.current[:__web_console_exception_lock] = false
-        end
+  define_method :set_backtrace do |*args|
+    unless Thread.current[:__web_console_exception_lock]
+      Thread.current[:__web_console_exception_lock] = true
+      begin
+        @__web_console_bindings_stack = binding.callers.drop(1)
+      ensure
+        Thread.current[:__web_console_exception_lock] = false
       end
-
-      original_set_backtrace.bind(self).call(*args)
     end
+
+    original_set_backtrace.bind(self).call(*args)
   end
 
   def __web_console_bindings_stack
