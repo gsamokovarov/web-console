9a1785e5c602f62bccff28ab18b15f35c70a46e8
Byron Bischoff <byronb@gmail.com>
2014-10-23 10:19:17 -0700

    Fixed naming in routes for tests_controller

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get :helper_test, to: "helper_test#index"
   get :controller_helper_test, to: "controller_helper_test#index"
 
-  resource :test do
+  namespace :tests do
     get :render_console_ontop_of_text
     get :renders_console_only_once
     get :doesnt_render_console_on_non_html_requests
