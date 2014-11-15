27793765a7bb7678828b9e21c21b465ac7e3faa5
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-11-15 17:51:52 +0200

    Format console errors similarly to IRB
    Currently, if an exception is raised during input console input sending
    it will look like:
    
    ```
    >> raise
    !! RuntimeError
    ```
    
    Now, we can show a backtrace in a similar format to IRB. It will be
    cleaned up of the framework traces until the REPL for noiseless
    debugging.
    
    ```
    >> request.missing
    NoMethodError: undefined method `missing' for #<ActionDispatch::Request:0x007fb6228197e8>
    	from /web-console/test/dummy/app/controllers/exception_test_controller.rb:13:in `test_method'
    ```

--- lib/web_console/repl.rb
+++ lib/web_console/repl.rb
@@ -1,9 +1,13 @@
 module WebConsole
-  # Simple REPL implementation.
-  # It uses @binding.eval for code evaluation.
-  # No styling or console configurations is supported.
+  # Simple read–eval–print implementation.
+  #
+  # Provides only the most basic code evaluation with no multiline code
+  # support.
   class REPL
-    attr_accessor :binding
+    # Cleanses exceptions raised inside #send_input.
+    cattr_reader :cleaner
+    @@cleaner = ActiveSupport::BacktraceCleaner.new
+    @@cleaner.add_silencer { |line| line.start_with?(File.expand_path('..', __FILE__)) }
 
     def initialize(binding = TOPLEVEL_BINDING)
       @binding = binding
@@ -14,11 +18,19 @@ module WebConsole
     end
 
     def send_input(input)
-      eval_result = nil
-      eval_result = @binding.eval(input).inspect
-      "=> #{eval_result}\n"
+      "=> #{@binding.eval(input).inspect}\n"
     rescue Exception => exc
-      "!! #{exc.inspect rescue exc.class.to_s rescue "Exception"}\n"
+      format_exception(exc)
     end
+
+    private
+
+      def format_exception(exc)
+        backtrace = cleaner.clean(Array(exc.backtrace) - caller)
+
+        format = "#{exc.class.name}: #{exc}\n"
+        format << backtrace.map { |trace| "\tfrom #{trace}\n" }.join
+        format
+      end
   end
 end
