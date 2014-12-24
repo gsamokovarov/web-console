fe98771d68e86eccc83b008d0da4360ddf2a7000
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-12-24 16:32:36 +0200

    Revamp WebConsole::Request documentation

--- lib/web_console/request.rb
+++ lib/web_console/request.rb
@@ -1,14 +1,18 @@
 module WebConsole
   # Web Console tailored request object.
   class Request < ActionDispatch::Request
-    # For a request to hit Web Console features, it needs to come from a
-    # white listed IP and to be XMLHttpRequest.
+    # Returns whether a request came from a whitelisted IP.
+    #
+    # For a request to hit Web Console features, it needs to come from a white
+    # listed IP.
     def from_whitelited_ip?
       WebConsole.config.whitelisted_ips.include?(remote_ip)
     end
 
-    # An acceptable content type for Web Console is HTML only.
-    # If a client didn't specified it, we'll assume its HTML.
+    # Returns whether the request is from an acceptable content type.
+    #
+    # We can render a console only for HTML. If a client didn't specified any,
+    # we'll assume its HTML, because this is common.
     def acceptable_content_type?
       content_type.blank? || Mime::HTML == content_type
     end
