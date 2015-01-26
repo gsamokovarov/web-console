40357ecb718f2f0708eea3461ef63052b18eafbc
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-01-26 22:43:44 +0200

    Accept text/plain as acceptable content type for Puma
    While most of the servers will return blank content type if none given,
    Puma will return text/plain.
    
    This rendered the console unusable for Puma users.

--- lib/web_console/request.rb
+++ lib/web_console/request.rb
@@ -1,6 +1,10 @@
 module WebConsole
   # Web Console tailored request object.
   class Request < ActionDispatch::Request
+    # While most of the servers will return blank content type if none given,
+    # Puma will return text/plain.
+    ACCEPTABLE_CONTENT_TYPE = [Mime::HTML, Mime::TEXT]
+
     # Returns whether a request came from a whitelisted IP.
     #
     # For a request to hit Web Console features, it needs to come from a white
@@ -11,10 +15,10 @@ module WebConsole
 
     # Returns whether the request is from an acceptable content type.
     #
-    # We can render a console only for HTML. If a client didn't specified any,
-    # we'll assume its HTML, because this is common.
+    # We can render a console for HTML and TEXT. If a client didn't
+    # specified any content type, we'll render it as well.
     def acceptable_content_type?
-      content_type.blank? || Mime::HTML == content_type
+      content_type.blank? || content_type.in?(ACCEPTABLE_CONTENT_TYPE)
     end
   end
 end
