a05f96fbae2e7ff124de9cae9483ba5102a8a4ed
Hiroyuki Sano <sh19910711@gmail.com>
2015-06-21 20:08:27 +0900

    Add spec_helper.js to test/templates

---
+++ test/templates/spec/spec_helper.js
@@ -0,0 +1,25 @@
+(function() {
+  var SpecHelper = {
+    triggerEvent: function(el, eventName) {
+      var event = document.createEvent("MouseEvents");
+      event.initEvent(eventName, true, true); // type, bubbles, cancelable
+      el.dispatchEvent(event);
+    },
+    randomString: function() {
+      Math.random().toString(36).substring(2);
+    },
+    prepareStageElement: function() {
+      before(function() {
+        this.stageElement = document.createElement("div");
+        this.stageElement.style.display = "none";
+        document.body.appendChild(this.stageElement);
+      });
+      afterEach(function() {
+        this.stageElement.innerHTML = "";
+      });
+    }
+  };
+
+  window.SpecHelper = SpecHelper;
+  window.assert = chai.assert;
+})();
