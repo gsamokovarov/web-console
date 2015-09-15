b8814b52939bef7b4c95fd6da9dd0d96d1fb5e44
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-09-15 17:45:57 +0300

    Revamp the Rubinius integration
    * Implement WebConsole.caller_bindings instead of
      WebConsole::Rubinius.current_bindings.
    
    * Drop the WebConsole::Rubinius::InternalLocationFilter class and inline
      its implementation in WebConsole.caller_bindings.

--- lib/web_console/integration/rubinius.rb
+++ lib/web_console/integration/rubinius.rb
@@ -1,62 +1,34 @@
-module WebConsole
-  module Rubinius
-    # Filters internal Rubinius locations.
-    #
-    # There are a couple of reasons why we wanna filter out the locations.
-    #
-    # * ::Kernel.raise, is implemented in Ruby for Rubinius. We don't wanna
-    #   have the frame for it to align with the CRuby and JRuby implementations.
-    #
-    # * For internal methods location variables can be nil. We can't create a
-    #   bindings for them.
-    #
-    # * Bindings from the current file are considered internal and ignored.
-    #
-    # We do that all that so we can align the bindings with the backtraces
-    # entries.
-    class InternalLocationFilter
-      def initialize(locations)
-        @locations = locations
-      end
+def WebConsole.caller_bindings
+  locations = ::Rubinius::VM.backtrace(1, true)
 
-      def filter
-        @locations.reject do |location|
-          location.file.start_with?('kernel/delta/kernel.rb') ||
-            location.file == __FILE__ ||
-            location.variables.nil?
-        end
-      end
-    end
-
-    # Gets the current bindings for all available Ruby frames.
-    #
-    # Filters the internal Rubinius and WebConsole frames.
-    def self.current_bindings
-      locations = ::Rubinius::VM.backtrace(1, true)
-
-      InternalLocationFilter.new(locations).filter.map do |location|
-        Binding.setup(
-          location.variables,
-          location.variables.method,
-          location.constant_scope,
-          location.variables.self,
-          location
-        )
-      end
-    end
+  # Kernel.raise, is implemented in Ruby for Rubinius. We don't wanna have
+  # the frame for it to align with the CRuby and JRuby implementations.
+  #
+  # For internal methods location variables can be nil. We can't create a
+  # bindings for them.
+  locations.reject! do |location|
+    location.file.start_with?('kernel/delta/kernel.rb') || location.variables.nil?
   end
-end
 
-::Exception.class_eval do
-  def bindings
-    @bindings || []
+  bindings = locations.map do |location|
+    Binding.setup(
+      location.variables,
+      location.variables.method,
+      location.constant_scope,
+      location.variables.self,
+      location
+    )
   end
+
+  # Drop the binding of the direct caller. That one can be created by
+  # Kernel#binding.
+  bindings.drop(1)
 end
 
 ::Rubinius.singleton_class.class_eval do
   def raise_exception_with_current_bindings(exc)
     if exc.bindings.empty?
-      exc.instance_variable_set(:@bindings, WebConsole::Rubinius.current_bindings)
+      exc.instance_variable_set(:@bindings, WebConsole.caller_bindings)
     end
 
     raise_exception_without_current_bindings(exc)
