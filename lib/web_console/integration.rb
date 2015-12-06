ca3b38a0c8c3ecd97f6e97214015877f015c3b0b
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-12-06 16:27:56 +0200

    Remove JRuby support for 3.0
    I want to revisit this, but I have to extract the hacky bindex
    implementation for JRuby 9k. Will do this for 3.1 since we never saw
    that much JRuby usage for the effort we put into supporting it.
    
    I still want to support it, it's just not a priority at the moment.

--- lib/web_console/integration.rb
+++ lib/web_console/integration.rb
@@ -26,8 +26,6 @@ end
 case RUBY_ENGINE
 when 'rbx'
   require 'web_console/integration/rubinius'
-when 'jruby'
-  require 'web_console/integration/jruby'
 when 'ruby'
   require 'web_console/integration/cruby'
 end
