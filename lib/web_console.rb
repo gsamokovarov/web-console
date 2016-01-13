c57c809863e4660e7bd85c4a8746475cf625a5f3
Herminio Torres <herminiocesar@gmail.com>
2016-01-13 03:18:00 -0200

    Improve boot
    With this change decrease boot time, where the files
    will only be called when it is actually used.

--- lib/web_console.rb
+++ lib/web_console.rb
@@ -4,18 +4,18 @@ require 'active_support/logger'
 require 'web_console/integration'
 require 'web_console/railtie'
 require 'web_console/errors'
-require 'web_console/helper'
-require 'web_console/evaluator'
-require 'web_console/session'
 require 'web_console/template'
 require 'web_console/middleware'
 require 'web_console/whitelist'
 require 'web_console/request'
-require 'web_console/response'
-require 'web_console/whiny_request'
 
 module WebConsole
   autoload :View, 'web_console/view'
+  autoload :Helper, 'web_console/helper'
+  autoload :Evaluator, 'web_console/evaluator'
+  autoload :Session, 'web_console/session'
+  autoload :Response, 'web_console/response'
+  autoload :WhinyRequest, 'web_console/whiny_request'
 
   mattr_accessor :logger
   @@logger = ActiveSupport::Logger.new($stderr)
