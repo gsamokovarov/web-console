d3b962948faa82890deaef8bca96b7cdad9d89d8
Hiroyuki Sano <sh19910711@gmail.com>
2016-07-27 00:47:48 +0000

    Move refining tests for auto-completion into a test suite

--- test/templates/test/auto_complete_test.js
+++ test/templates/test/auto_complete_test.js
@@ -1,58 +1,60 @@
 suite('Autocomplete', function() {
-  setup(function() {
-    this.ac = new Autocomplete(['other', 'other2', 'something', 'somewhat', 'somewhere', 'test']);
-    this.refine = function(prefix) { this.ac.refine(prefix); };
-  });
-
-  test('does nothing if the word list is empty', function() {
-    var ac = new Autocomplete([]);
-    assert.doesNotThrow(function() {
-      ac.onKeyDown(TestHelper.keyDown(TestHelper.KEY_TAB));
-      ac.onKeyDown(TestHelper.keyDown(TestHelper.KEY_ENTER));
+  suite('Refinements', function() {
+    setup(function() {
+      this.ac = new Autocomplete(['other', 'other2', 'something', 'somewhat', 'somewhere', 'test']);
+      this.refine = function(prefix) { this.ac.refine(prefix); };
     });
-  });
 
-  test('empty string', function() {
-    this.refine('');
+    test('does nothing if the word list is empty', function() {
+      var ac = new Autocomplete([]);
+      assert.doesNotThrow(function() {
+        ac.onKeyDown(TestHelper.keyDown(TestHelper.KEY_TAB));
+        ac.onKeyDown(TestHelper.keyDown(TestHelper.KEY_ENTER));
+      });
+    });
 
-    assert(!this.ac.confirmed);
-    assertRange(this, 0, this.ac.words.length, 0);
-  });
+    test('empty string', function() {
+      this.refine('');
 
-  test('some', function() {
-    this.refine('some');
+      assert(!this.ac.confirmed);
+      assertRange(this, 0, this.ac.words.length, 0);
+    });
 
-    assert(!this.ac.confirmed);
-    assertRange(this, 2, this.ac.words.length - 1, 3);
-  });
+    test('some', function() {
+      this.refine('some');
 
-  test('confirmable', function() {
-    this.refine('somet');
+      assert(!this.ac.confirmed);
+      assertRange(this, 2, this.ac.words.length - 1, 3);
+    });
 
-    assert.equal(this.ac.confirmed, 'something');
-    assertRange(this, 2, 3);
-  });
+    test('confirmable', function() {
+      this.refine('somet');
 
-  test('decrement', function() {
-    this.refine('o');
-    this.refine('');
+      assert.equal(this.ac.confirmed, 'something');
+      assertRange(this, 2, 3);
+    });
 
-    assertRange(this, 0, this.ac.words.length, 0);
-  });
+    test('decrement', function() {
+      this.refine('o');
+      this.refine('');
 
-  test('other => empty => some', function() {
-    this.refine('other');
-    this.refine('');
-    this.refine('some');
+      assertRange(this, 0, this.ac.words.length, 0);
+    });
 
-    assertRange(this, 2, this.ac.words.length - 1, 3);
-  });
+    test('other => empty => some', function() {
+      this.refine('other');
+      this.refine('');
+      this.refine('some');
 
-  test('some => empty', function() {
-    this.refine('some');
-    this.refine('');
+      assertRange(this, 2, this.ac.words.length - 1, 3);
+    });
 
-    assertRange(this, 0, this.ac.words.length);
+    test('some => empty', function() {
+      this.refine('some');
+      this.refine('');
+
+      assertRange(this, 0, this.ac.words.length);
+    });
   });
 
   function assertRange(self, left, right, hiddenCnt) {
