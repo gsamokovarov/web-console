5a9b1d0b9682811ba306a7785d8159f5f9370802
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-03-30 22:58:42 +0300

    Fix the deprecated mocha/mini_test require

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -77,7 +77,7 @@ if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
 
-require "mocha/mini_test"
+require "mocha/minitest"
 
 module External
   def self.exception
