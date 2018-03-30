7b5b40328ffd3b1d798224570402297c905951c3
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-03-30 17:30:35 +0300

    Fix frozen string modifications in WebConsole::Response

--- lib/web_console/response.rb
+++ lib/web_console/response.rb
@@ -12,13 +12,12 @@ module WebConsole
       # We're done with the original body object, so make sure to close it to comply with the Rack SPEC
       body.close if body.respond_to?(:close)
 
-      if position = raw_body.rindex('</body>')
-        raw_body.insert(position, content)
-      else
-        raw_body << content
-      end
-
-      self.body = raw_body
+      self.body =
+        if position = raw_body.rindex('</body>')
+          raw_body.dup.insert(position, content)
+        else
+          raw_body.dup << content
+        end
     end
 
     def finish
