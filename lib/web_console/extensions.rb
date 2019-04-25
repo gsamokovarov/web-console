96127aac143e1e653fffdc4bb65e1ce0b5ff342d
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-04-25 22:32:30 +0300

    Introduce Binding#console
    This interface can spawn a console a bit more quickly as it doesn't need
    to pry open the stack frames.

--- lib/web_console/extensions.rb
+++ lib/web_console/extensions.rb
@@ -8,8 +8,8 @@ module Kernel
   # If +binding+ isn't explicitly given it will default to the binding of the
   # previous frame. E.g. the one that invoked +console+.
   #
-  # Raises DoubleRenderError if a double +console+ invocation per request is
-  # detected.
+  # Raises +DoubleRenderError+ if a more than one +console+ invocation per
+  # request is detected.
   def console(binding = Bindex.current_bindings.second)
     raise WebConsole::DoubleRenderError if Thread.current[:__web_console_binding]
 
@@ -19,5 +19,16 @@ module Kernel
     # you're gonna see unexpected #<Binding:0x007fee4302b078> in the
     # templates.
     nil
+  end
+end
+
+class Binding
+  # Instructs Web Console to render a console in the current binding, without
+  # the need to unroll the stack.
+  #
+  # Raises +DoubleRenderError+ if a more than one +console+ invocation per
+  # request is detected.
+  def console
+    Kernel.console(self)
   end
 end
